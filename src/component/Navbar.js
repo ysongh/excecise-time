571483a3d3a57e283a1a2db60839c2079426e998
You Song Hou <yousonghou@yahoo.com>
2019-11-23 15:08:00 -0500

    add styles to the navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,12 +5,16 @@ const Navbar = () => {
     return(
         <nav>
             <div className="nav">
-            <Link to="/">
+            <Link to="/" className="nav__link">
                 <p>Immersive VRT</p>
             </Link>
-            <ul className="nav__linksGroup">
-                <li><Link to="/">DashBoard</Link></li>
-                <li><Link to="/graph">Graphs</Link></li>
+            <ul className="nav__list">
+                <li className="nav__item">
+                    <Link to="/" className="nav__link">DashBoard</Link>
+                </li>
+                <li className="nav__item">
+                    <Link to="/graph" className="nav__link">Graphs</Link>
+                </li>
             </ul>
             </div>
         </nav>
