d247ca7bf96d9b7e255a2a25bc303ba7b23588a8
You Song Hou <yousonghou@yahoo.com>
2019-11-23 11:58:25 -0500

    add logic to change the list based on the type

--- src/component/DashBoard.js
+++ src/component/DashBoard.js
@@ -10,6 +10,25 @@ class DashBoard extends Component{
         const value = e.target.value;
         
         this.setState({ selectType: value });
+        this.changetype(value);
+    }
+
+    changetype(value){
+        if(value === ""){
+            this.setState({ typeList: [1, 2, 3, 4, 5, 6, 7, 8, 9] });
+        }
+        else if(value === "head"){
+            this.setState({ typeList: [1, 2, 4, 5, 7, 9] });
+        }
+        else if(value === "eye"){
+            this.setState({ typeList: [5, 9] });
+        }
+        else if(value === "neck"){
+            this.setState({ typeList: [2, 4, 5, 7, 9] });
+        }
+        else if(value === "shoulder"){
+            this.setState({ typeList: [1, 4, 5, 8] });
+        }
     }
 
     render(){
@@ -27,6 +46,7 @@ class DashBoard extends Component{
                         <option value="">All</option>
                         <option value="head">Head</option>
                         <option value="eye">Eye</option>
+                        <option value="neck">Neck</option>
                         <option value="shoulder">Shoulder</option>
                     </select>
                 </div>
