db8a437501a9b7e4d15b47c23ad4c8e250926b01
You Song Hou <yousonghou@yahoo.com>
2019-11-23 11:51:26 -0500

    use map to show the list

--- src/component/DashBoard.js
+++ src/component/DashBoard.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 
 class DashBoard extends Component{
     state = {
-        selectType: ""
+        selectType: "",
+        typeList: [1, 2, 3, 4, 5, 6, 7, 8, 9]
     }
 
     onSelect(e){
         const value = e.target.value;
         
-        this.setState({ selectType: value })
+        this.setState({ selectType: value });
     }
 
     render(){
+
         return(
             <div className="dashBoard">
                 <h1 className="dashBoard__title">Patient AI</h1>
@@ -29,60 +31,14 @@ class DashBoard extends Component{
                     </select>
                 </div>
                 <div className="dashBoard__checkboxArea">
-                    <div className="dashBoard__checkboxGroup">
-                        <p>Exercise #1</p>
-                        <input
-                            type="checkbox"
-                            name="checkbox1"/>
-                    </div>
-                    <div className="dashBoard__checkboxGroup">
-                        <p>Exercise #2</p>
-                        <input
-                            type="checkbox"
-                            name="checkbox2"/>
-                    </div>
-                    <div className="dashBoard__checkboxGroup">
-                        <p>Exercise #3</p>
-                        <input
-                            type="checkbox"
-                            name="checkbox3"/>
-                    </div>
-                    <div className="dashBoard__checkboxGroup">
-                        <p>Exercise #4</p>
-                        <input
-                            type="checkbox"
-                            name="checkbox4"/>
-                    </div>
-                    <div className="dashBoard__checkboxGroup">
-                        <p>Exercise #5</p>
-                        <input
-                            type="checkbox"
-                            name="checkbox5"/>
-                    </div>
-                    <div className="dashBoard__checkboxGroup">
-                        <p>Exercise #6</p>
-                        <input
-                            type="checkbox"
-                            name="checkbox6"/>
-                    </div>
-                    <div className="dashBoard__checkboxGroup">
-                        <p>Exercise #7</p>
-                        <input
-                            type="checkbox"
-                            name="checkbox7"/>
-                    </div>
-                    <div className="dashBoard__checkboxGroup">
-                        <p>Exercise #8</p>
-                        <input
-                            type="checkbox"
-                            name="checkbox8"/>
-                    </div>
-                    <div className="dashBoard__checkboxGroup">
-                        <p>Exercise #9</p>
-                        <input
-                            type="checkbox"
-                            name="checkbox9"/>
-                    </div>
+                    {this.state.typeList.map(type => {
+                        return <div className="dashBoard__checkboxGroup">
+                            <p>Exercise #{type}</p>
+                                <input
+                                    type="checkbox"
+                                    name="checkbox1"/>
+                            </div>
+                    })}
                 </div>
             </div>
         )
