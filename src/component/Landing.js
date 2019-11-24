99c7009f9c292322156a8fbf681982bd9eae044c
You Song Hou <yousonghou@yahoo.com>
2019-11-23 23:18:22 -0500

    fix the position of the image on the landing page

--- src/component/Landing.js
+++ src/component/Landing.js
@@ -7,7 +7,7 @@ const Landing = () =>{
     return(
         <div className="landing">
             <h1 className="dashBoard__title">Weclcome to Immersive VRT</h1>
-            <img src={Image} alt="Markup" className="mt-2" />
+            <img src={Image} alt="Markup" className="dashBoard__img mt-2" />
             <Link to="/dashboard" className="landing__cta">
                 Dashboard
             </Link>
