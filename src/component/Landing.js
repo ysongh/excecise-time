e26344c5348c597534d07e25662d595da87862e6
You Song Hou <yousonghou@yahoo.com>
2019-11-24 10:53:13 -0500

    add more information on the landing page

--- src/component/Landing.js
+++ src/component/Landing.js
@@ -7,7 +7,8 @@ const Landing = () =>{
     return(
         <div className="landing">
             <h1 className="dashBoard__title">Weclcome to Immersive VRT</h1>
-            <img src={Image} alt="Markup" className="dashBoard__img mt-2" />
+            <img src={Image} alt="Markup" className="landing__img mt-2" />
+            <p className="landing__p">Track Your Balance</p>
             <Link to="/dashboard" className="landing__cta">
                 Dashboard
             </Link>
